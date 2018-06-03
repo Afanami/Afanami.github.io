df2084f47fe323092b405e84d817c80dd70818b3
Afanami <afan6333@gmail.com>
2018-06-03 18:56:08 +1200

    added menu toggling

--- dist/js.46c0e84c.js
+++ dist/js.46c0e84c.js
@@ -98,9 +98,9 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   // Override the current require with this new one
   return newRequire;
-})({15:[function(require,module,exports) {
+})({7:[function(require,module,exports) {
 
-},{}],18:[function(require,module,exports) {
+},{}],29:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -129,7 +129,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49758' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49394' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -270,4 +270,4 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[18,15], null)
+},{}]},{},[29,7], null)
